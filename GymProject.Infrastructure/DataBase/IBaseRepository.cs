d7fd74f025e430d131e11028d8d3c93ed89c1ce1
Настя <@DESKTOP-LRIPME3>
2023-11-25 13:18:36 +0300

    may

---
+++ GymProject.Infrastructure/DataBase/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymProject.Infrastructure.DataBase
+{
+    internal interface IBaseRepository<TEntity>
+    {
+        TEntity GetById(long id);
+        List<TEntity> GetList();
+    }
+
+}
